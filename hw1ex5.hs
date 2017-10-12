68cac34deabbbabd1c682eeb8cd6fba61553d5ff
naraic <naraic0o@gmail.com>
2017-10-12 15:46:08 +0100

    exercise 5 working. not doing exercise 6...for now

---
+++ hw1ex5.hs
@@ -0,0 +1,7 @@
+--type synon
+type Peg = String
+type Move = (Peg, Peg)
+
+hanoi :: Integer -> Peg -> Peg -> Peg -> [Move]
+hanoi 1 a b c = [(a, b)]
+hanoi n a b c = hanoi (n-1) a c b ++ hanoi 1 a b c ++ hanoi (n-1) c b a
