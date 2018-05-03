9ea9ef2c8d9a1905a906515a95c7cfe01c1f9fff
naraic <naraic0o@gmail.com>
2018-05-03 14:59:09 +0100

    hw4 ex2 done, eventually. that where though. kinda gross.

--- week4/hw4ex2.hs
+++ week4/hw4ex2.hs
@@ -7,15 +7,17 @@ foldTree = foldr insert Leaf
 
 insert :: a -> Tree a -> Tree a
 insert m Leaf = Node 0 Leaf m Leaf
-insert m (Node h Leaf n Leaf)   = Node (h+1) (insert m Leaf) n Leaf 
-insert m (Node h l n Leaf)      = Node h l n (insert m Leaf)
-insert m (Node h l@(Node lh _ _ _) n r@(Node rh _ _ _)) -- with lh satanding for left height etc
-    | lh == rh  = (Node (h+1) (insert m l) n r)
-    | otherwise = (Node h l n (insert m r))
+insert m t@(Node h l n r) 
+    | ll > lr                = (Node h l n (insert m r)) 
+    | lc == lf               = (Node (h+1) (insert m l) n r) 
+    | otherwise              = (Node h (insert m l) n r)
+    where 
+        ll = leaves l
+        lr = leaves r
+        l2 = logBase 2.0 (fromIntegral ll)
+        lc = ceiling l2
+        lf = floor l2
 
-
-
-
-
---foldr :: (a -> b -> b) -> b -> t a -> b 
---foldr f z [x1, x2, ..., xn] == x1 `f` (x2 `f` ... (xn `f` z)...)
+leaves :: Tree a -> Integer
+leaves Leaf                 = 1
+leaves (Node _ r    _ l   ) = leaves l + leaves r
