769e87ecfe14ffe6936a04eb44f2247647a8f5ad
naraic <naraic0o@gmail.com>
2018-05-03 17:16:47 +0100

    week4 exercise 3 part 2 complete

--- week4/hw4ex3.hs
+++ week4/hw4ex3.hs
@@ -1,3 +1,5 @@
+--1
+
 xor :: [Bool] -> Bool
 xor = foldr exclusiveOr False
 
@@ -6,3 +8,9 @@ exclusiveOr a b
     | a && b = False
     | (not a) && (not b) = False
     | otherwise = True
+
+
+--2
+
+map' :: (a -> b) -> [a] -> [b]
+map' f = foldr (\ x -> ((f x):)) []
