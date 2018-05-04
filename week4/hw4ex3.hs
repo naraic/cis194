6783a4cd608c60ddf8aa8f2bbcc0cc51672440aa
naraic <naraic0o@gmail.com>
2018-05-04 10:29:28 +0100

    hw4 ex3 complete - all parts

--- week4/hw4ex3.hs
+++ week4/hw4ex3.hs
@@ -14,3 +14,14 @@ exclusiveOr a b
 
 map' :: (a -> b) -> [a] -> [b]
 map' f = foldr (\ x -> ((f x):)) []
+
+
+--3
+
+--foldr :: (a -> b -> b) -> b -> t a -> b 
+
+myFoldl :: (a -> b -> a) -> a -> [b] -> a
+myFoldl f base xs = foldr (swap f) base (reverse xs)
+
+swap :: (a -> b -> a) -> (b -> a -> a) 
+swap f x y = f y x
