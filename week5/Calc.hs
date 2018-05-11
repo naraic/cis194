5ef6d8fbe0e3e73ee70ff674ee174b3ce183e7bf
naraic <naraic0o@gmail.com>
2018-05-11 12:08:30 +0100

    ex2 complete

--- week5/Calc.hs
+++ week5/Calc.hs
@@ -1,7 +1,16 @@
+module Calc where
 import ExprT
+import Parser
 
 eval :: ExprT -> Integer
 eval (Lit n) = n
 eval (Mul e1 e2) = (eval e1) * (eval e2)
 eval (Add e1 e2) = (eval e1) + (eval e2)
+
+evalStr :: String -> Maybe Integer
+evalStr = maybeEval . (parseExp Lit Add Mul)
+
+maybeEval :: Maybe ExprT -> Maybe Integer
+maybeEval Nothing = Nothing
+maybeEval (Just e) = Just (eval e)
 
