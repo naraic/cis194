68eb20762fe925ef4b7c1c7bfd56ed89376e9a64
naraic <naraic0o@gmail.com>
2018-12-03 21:01:44 +0000

    hw08 finally finished

--- week8/Party.hs
+++ week8/Party.hs
@@ -1,4 +1,8 @@
 module Party where
+import Employee
+
+import Data.Tree
+import Data.List
 
 glCons :: Employee -> GuestList -> GuestList
 glCons e@(Emp {empFun = f}) (GL es fun) = GL (e:es) (fun+f)
@@ -8,14 +12,28 @@ instance Monoid GuestList where
     mappend (GL es1 f1) (GL es2 f2) = GL (es1 ++ es2) (f1 + f2)
 
 moreFun :: GuestList -> GuestList -> GuestList
-moreFun gl1@(GL _ f1) gl2@(GL _ f2)
-    | f1 > f2 = gl1
-    | otherwise = gl2
+moreFun = max
 
-treeFold :: (Tree a -> b -> b) -> b -> Tree a -> b
-treeFold f z (Node val forest)
-    | forest == [] = z
-    | otherwise = f val . foldr (treeFold f z) z forest
+treeFold :: (a -> [b] -> b) -> Tree a -> b
+treeFold f (Node val forest) = f val $ map (treeFold f) forest
 
+nextLevel :: Employee -> [(GuestList, GuestList)] -> (GuestList, GuestList)
+nextLevel boss guestLists = (withBoss, withoutBoss)
+  where 
+    withBoss = glCons boss $ mconcat $ map snd guestLists
+    withoutBoss = mconcat $ map (uncurry moreFun) guestLists
 
+maxFun :: Tree Employee -> GuestList
+maxFun = uncurry moreFun . treeFold nextLevel
 
+pprint :: GuestList -> String
+pprint (GL es f) = "fun " ++ show f ++ "\n" ++ employees
+  where
+    fun = f
+    employees = unlines $ sort $ map empName es
+
+main :: IO ()
+main = do 
+  company <- readFile "company.txt"
+  let gl = maxFun $ read company
+  putStr $ pprint gl 
